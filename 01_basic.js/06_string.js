6eb68dd5effad951b67b205c8d6293efe9c3b351
chandan kumar <155745407+chandansharma7@users.noreply.github.com>
2025-05-01 09:06:05 +0000

    dates

---
+++ 01_basic.js/06_string.js
@@ -0,0 +1,25 @@
+const name ="chandan"
+const repoCount =50
+console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+const gamename = new String ('chandan_hr_new');
+// console.log(gamename[0]);
+
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+
+// const newString = gamename.substring(0 , 4)
+// console.log(newString);
+
+// const anotherstring = gamename.slice(-8,4)
+// console.log(anotherstring);
+
+const newString = "   chadnan.  "
+console.log(newString);
+console.log(newString.trim());
+
+const url = "chandan.com%20sharma"
+console.log(url.replace('%20','_'));
+console.log(url.includes("sundar"))
+console.log(gamename.split("_"));
+
---
+++ 01_basic.js/08_datesin.js
@@ -0,0 +1,11 @@
+//dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+
+let mucreatedate = new Date(2025 , 3 , 30)
+console.log(mucreatedate.toDateString());
