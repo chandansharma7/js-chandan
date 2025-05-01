bcfefe6e204adc508346f8cbbf927114a0d14974
chandan kumar <155745407+chandansharma7@users.noreply.github.com>
2025-05-01 07:18:32 +0000

    comparison

---
+++ 01_basic.js/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score ="33"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInnumber = Number (score)
+// console.log(typeof valueInnumber);
+
+
+let isloggeIn= 1;
+let booleanIsloogedIn = Boolean(isloggeIn)
+// console.log(booleanIsloogedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"chadna" => true
+
+//*************Operation**************
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(+true);
+// console.log(true+);
+
+let num1 , num2 ,num3
+num1 =num2 =num3 = 2+2
+let gameCounter =19
+console.log(+gameCounter);
+
---
+++ 01_basic.js/04_comparision.js
@@ -0,0 +1,16 @@
+// console.log("2"> 1);
+// console.log("02"> 1);
+
+// console.log(null > 0);  //output is false
+// console.log(null == 0); //output is false
+// console.log(null >= 0); //output is true
+
+console.log(undefined == 0); //output is false
+console.log(undefined > 0);//output is false
+console.log(undefined < 0);//output is false
+console.log("2" === 2);//output is false
+
+
+
+
+
