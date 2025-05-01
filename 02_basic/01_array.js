c7368b337d7a42eb563fa7059b6f76b1b0fa743a
chandan kumar <155745407+chandansharma7@users.noreply.github.com>
2025-05-01 09:40:40 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,14 @@
+// array
+
+const myarr =[1,2,3,4]
+const myHeros = ["skatiman", "capitan amirca" ,"nagraj "]
+// console.log(myarr[1]);
+
+
+// array methods
+myarr.push(6)
+// console.log(myarr);
+
+// myarr.unshift(9)
+// myarr.shift() 
+console.log(myarr);
