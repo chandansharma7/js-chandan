b148291d5c78e7e66966202fada38b31c5b65df0
chandan kumar <155745407+chandansharma7@users.noreply.github.com>
2025-05-01 06:27:27 +0000

    datatype

---
+++ 01_basic.js/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =1234
+let accountemail ="chadnan@gmail.com"
+var accountpasssword ="1233"
+accouncity = "noida"
+
+// accountId = 2
+accountemail ="nodtjo@gmail.com"
+accountpasssword = "344"
+let personname;
+/*
+prefer not to use var 
+beacause of issur in block scope and functional sc
+*/
+
+console.log(accountId);
+console.log(accountemail);
+console.log(accountpasssword);
+console.table([accountId,accountemail,accountpasssword,accouncity,personname])
---
+++ 01_basic.js/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as new version
+
+let string = "chandan"
+let age =22
+let isloggedIn =false
+let state;
+
+//number => 2 to power 53
+//bigint using for big number
+//string => ""
+//boolearn =>. True or False
+// null => standalone value
+//undefined => value not assign 
+//symbol => unique
+
+
+//object 
+
+console.log(typeof null);
+console.log(typeof undefined);
